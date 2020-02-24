f99361287ac76f9edcc462ab653edf2f423c6550
Artur Manukian <manukian.artur.94@gmail.com>
2020-02-24 20:53:08 +0200

    feature: add notification endpoints

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const config = require('./config');
+const postHandler = require('./handlers/post');
+const putHandler = require('./handlers/put');
+const deleteHandler = require('./handlers/delete');
 
 const app = express();
 const { PORT } = config;
@@ -9,6 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.sendStatus(200));
+
+app.post('/notifications', postHandler);
+app.put('/notifications/:id', putHandler);
+app.delete('/notifications/:id', deleteHandler);
 
 app.listen(PORT, () => {
   console.log(`Notification service is running on ${PORT}`);
