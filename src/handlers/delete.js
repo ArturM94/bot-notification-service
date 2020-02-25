b4579b125b608f3e3d0e905c6945d6a3b0a318ba
Artur Manukian <manukian.artur.94@gmail.com>
2020-02-26 00:58:52 +0200

    feature: add logic delete job handler

--- src/handlers/delete.js
+++ src/handlers/delete.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 
+const queue = require('../queue');
+
 module.exports.delete = async (req, res) => {
   try {
     const errors = validationResult(req);
@@ -8,6 +10,17 @@ module.exports.delete = async (req, res) => {
       console.log(errors);
       res.status(422).json({ errors: errors.array() });
     }
+
+    const { id } = req.params;
+    const job = await queue.getJob(id);
+
+    if (job === null) {
+      res.status(404).json({ error: 'Job not found' });
+    }
+
+    await job.remove();
+
+    res.status(200).json({ message: 'Job has been removed successfully' });
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
