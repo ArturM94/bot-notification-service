cf87d2a4faa4babd502b4906adbb448e6cb58fdc
Artur Manukian <manukian.artur.94@gmail.com>
2020-02-26 00:58:33 +0200

    feature: add logic put job handler

--- src/handlers/put.js
+++ src/handlers/put.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 
+const { queue } = require('../queue');
+
 module.exports.put = async (req, res) => {
   try {
     const errors = validationResult(req);
@@ -8,6 +10,18 @@ module.exports.put = async (req, res) => {
       console.log(errors);
       res.status(422).json({ errors: errors.array() });
     }
+
+    const { id } = req.params;
+    const { body } = req;
+    const job = await queue.getJob(id);
+
+    if (job === null) {
+      res.status(404).json({ error: 'Job not found' });
+    }
+
+    await job.update(body);
+
+    res.status(200).json({ message: 'Job has been updated successfully' });
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
