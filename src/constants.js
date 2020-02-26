9e687d0a2f9a1754f1cc385dfc3078bd62583e02
Artur Manukian <manukian.artur.94@gmail.com>
2020-02-26 20:54:06 +0200

    feature: add JOB_TYPES constant

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+const JOB_TYPES = {
+  WAITING: 'waiting',
+  ACTIVE: 'active',
+  COMPLETED: 'completed',
+  FAILED: 'failed',
+  DELAYED: 'delayed',
+  PAUSED: 'paused',
+};
+
+module.exports = { JOB_TYPES };
