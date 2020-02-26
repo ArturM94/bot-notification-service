c9c6e6fe3c466a6e0b8e575fe735d6d82a2dd0bf
Artur Manukian <manukian.artur.94@gmail.com>
2020-02-26 21:16:55 +0200

    refactor: validation messages

--- src/validators.js
+++ src/validators.js
@@ -5,33 +5,33 @@ const { JOB_TYPES } = require('./constants');
 const jobTypes = Object.values(JOB_TYPES);
 
 const idValidator = [
-  param('id', 'ID is wrong. Should not be empty')
+  param('id', 'id should not be empty')
     .not().isEmpty(),
-  param('id', 'ID is wrong. Should be a string')
+  param('id', 'id should be a string')
     .isString(),
 ];
 
 const bodyValidator = [
-  body('telegramId', 'Telegram ID is wrong. Should be a string')
+  body('telegramId', 'telegramId should be a string')
     .not().isEmpty()
     .isString(),
-  body('text', 'Text is wrong. Should be a string')
+  body('text', 'text should be a string')
     .not().isEmpty()
     .isString()
     .trim(),
-  body('attachments', 'Attachments is wrong. Should be a string')
+  body('attachments', 'attachments should be a string')
     .not().isEmpty()
     .isString()
     .trim(),
-  body('date', 'Date is wrong. Should comply with the ISO 8601 standard')
+  body('date', 'date should comply with the ISO 8601 standard')
     .not().isEmpty()
     .isISO8601(),
 ];
 
 const jobTypeValidator = [
-  query('jobType', 'Job type is wrong. Should not be empty')
+  query('jobType', 'jobType should not be empty')
     .not().isEmpty(),
-  query('jobType', `Job type is wrong. Should be one of ${jobTypes.join(', ')}`)
+  query('jobType', `jobType should be one of ${jobTypes.join(', ')}`)
     .isIn(jobTypes),
 ];
 
