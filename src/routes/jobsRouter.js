59b94658665105ab7d0622dd42f78b769d7c160b
Artur Manukian <manukian.artur.94@gmail.com>
2020-02-27 19:47:31 +0200

    refactor: move validators.js to helpers

--- src/routes/jobsRouter.js
+++ src/routes/jobsRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const jobController = require('../controllers');
-const { notificationValidator, idValidator, jobTypeValidator } = require('../validators');
+const { notificationValidator, idValidator, jobTypeValidator } = require('../helpers/validators');
 
 const jobsRouter = express.Router();
 
