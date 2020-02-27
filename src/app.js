d56d2cd97325acfa4e98556819497c1c165aab94
Artur Manukian <manukian.artur.94@gmail.com>
2020-02-27 19:43:40 +0200

    feature: add jobsRouter

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 const express = require('express');
 
 const config = require('./config');
-const postJob = require('./controllers/postJob');
-const putJob = require('./controllers/putJob');
-const deleteJob = require('./controllers/deleteJob');
-const getJobs = require('./controllers/getJobs');
-const { notificationValidator, idValidator, jobTypeValidator } = require('./validators');
+const jobsRouter = require('./routes/jobsRouter');
 
 const app = express();
 const { PORT } = config;
@@ -15,10 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.sendStatus(200));
 
-app.get('/jobs', jobTypeValidator, getJobs);
-app.post('/jobs', notificationValidator, postJob);
-app.put('/jobs/:id', idValidator, notificationValidator, putJob);
-app.delete('/jobs/:id', idValidator, deleteJob);
+app.use('/jobs', jobsRouter);
 
 app.listen(PORT, () => {
   console.log(`Notification service is running on ${PORT}`);
---
+++ src/routes/jobsRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const jobController = require('../controllers');
+const { notificationValidator, idValidator, jobTypeValidator } = require('../validators');
+
+const jobsRouter = express.Router();
+
+jobsRouter.get('/', jobTypeValidator, jobController.getJobs);
+jobsRouter.post('/', notificationValidator, jobController.postJob);
+jobsRouter.put('/:id', idValidator, notificationValidator, jobController.putJob);
+jobsRouter.delete('/:id', idValidator, jobController.deleteJob);
+
+module.exports = jobsRouter;
