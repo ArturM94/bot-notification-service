2aa645898a21ca1e2d685d8debc0cb4b30ee8429
Artur Manukian <manukian.artur.94@gmail.com>
2020-02-27 19:26:13 +0200

    feature: add index file for controllers

---
+++ src/controllers/index.js
@@ -0,0 +1,11 @@
+const getJobs = require('./getJobs');
+const postJob = require('./postJob');
+const putJob = require('./putJob');
+const deleteJob = require('./deleteJob');
+
+module.exports = {
+  getJobs,
+  postJob,
+  putJob,
+  deleteJob,
+};
