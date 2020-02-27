b42ce82cde693950cad0884999d269f7ef42bbd6
Artur Manukian <manukian.artur.94@gmail.com>
2020-02-28 00:56:22 +0200

    docs: add for calculateDelay function

--- src/helpers/calculateDelay.js
+++ src/helpers/calculateDelay.js
@@ -1,1 +1,7 @@
+/**
+ * Calculates delay.
+ *
+ * @param {string} date Date in ISO 8601 standard
+ * @return {number} Milliseconds
+ */
 module.exports.calculateDelay = (date) => Date.parse(date) - Date.now();
