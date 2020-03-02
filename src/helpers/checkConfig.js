51d08a34a5a06fb6056ad822a625a6f154efa4cc
Artur Manukian <manukian.artur.94@gmail.com>
2020-03-02 18:54:41 +0200

    refactor: if statement condition in checkConfig.js

--- src/helpers/checkConfig.js
+++ src/helpers/checkConfig.js
@@ -2,7 +2,7 @@ const { REQUIRED_ENV } = require('./constants');
 
 const checkConfig = (config) => {
   REQUIRED_ENV.forEach((item) => {
-    if (config[item] === undefined) {
+    if (!config[item]) {
       console.error(`The required environment variable "${item}" has not been defined!`);
       process.exit(1);
     }
