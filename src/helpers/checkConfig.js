eae35d7334da5eb79a7ef11d5e866d6312fb6e0a
Yehor Baklanov <baklanov199@gmail.com>
2020-03-02 16:37:58 +0300

    fix: fixed message text

--- src/helpers/checkConfig.js
+++ src/helpers/checkConfig.js
@@ -3,7 +3,7 @@ const { REQUIRED_ENV } = require('./constants');
 const checkConfig = (config) => {
   REQUIRED_ENV.forEach((item) => {
     if (config[item] === undefined) {
-      console.error(`The required environment variable "${item}" has not been defined`);
+      console.error(`The required environment variable "${item}" has not been defined!`);
       process.exit(1);
     }
   });
